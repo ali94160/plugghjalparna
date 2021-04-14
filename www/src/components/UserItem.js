b7dbbb474ae5ca3f03b2dc2e1d12daf24f482f33
Ali94160 <ali.al-mousawi@hotmail.com>
2021-04-14 22:20:28 +0200

    Created 35 accounts + styled UserList.
    Default picture for users if they havent chosen one.

--- www/src/components/UserItem.js
+++ www/src/components/UserItem.js
@@ -3,6 +3,7 @@ import '../style/UserItem.css'
 
 const UserItem = ({ user }) => {
 
+  const defaultIMG = "https://lh3.googleusercontent.com/proxy/y2rXhAQHDj7labWbTqIGqSoqko_grkSFbWmdkFDjl4DX98SY52idJDC4NPE5zOqxDjfCMlMmdpvBajce-VptEXGtph7UoHy3IVbre1Lr3kFlHGFfrCW7Hyyq"
 
 
 
@@ -10,9 +11,18 @@ const UserItem = ({ user }) => {
     <div className="userItemDiv">
 
       <div className="profileCard">
-        <h2>UserID: {user.firstName}</h2>
-        <h2>Name: {user.lastName}</h2>
-        <h2>role: {user.roles} </h2>
+        <div className="imageCard">
+          {user.profileImgURL ? <img src={user.profileImgURL} alt="" /> : <img  className="defaultIMG" src={defaultIMG} alt="" />}
+          <p>role: {user.roles} </p>
+        </div>
+        <div className="infoCard">
+          <p>{user.firstName}</p><span>{user.lastName}</span>
+          <p>{user.age}</p>
+          {user.lastTimeOnline ? <p>LOL{user.lastTimeOnline}</p> : <p>Online</p>}
+          <p>{user.country}</p>
+        </div>
+        
+       
           
       </div>
     </div>
