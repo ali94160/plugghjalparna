22b5d1f65ecb3b0e1a9a4c12ae5ba2f9cc676176
Ali94160 <ali.al-mousawi@hotmail.com>
2021-04-15 01:06:48 +0200

    Added 'profileImg' if user uploaded one.
    + css

--- www/src/components/UserItem.js
+++ www/src/components/UserItem.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style/UserItem.css'
 import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import RestoreRoundedIcon from '@material-ui/icons/RestoreRounded';
+import VerifiedUserRoundedIcon from '@material-ui/icons/VerifiedUserRounded';
 
 const UserItem = ({ user }) => {
 
   const defaultIMG = "https://i.postimg.cc/RCj6Y344/New-Project-7.png"
-
 
 
   return (
@@ -15,12 +15,12 @@ const UserItem = ({ user }) => {
 
       <div className="profileCard">
         <div className="imageCard">
-          {user.profileImgURL ? <img src={user.profileImgURL} alt="" /> : <img  className="defaultIMG" src={defaultIMG} alt="" />}
+          {user.profileImgURL ? <img className="profileIMG" src={user.profileImgURL} alt="" /> : <img  className="defaultIMG" src={defaultIMG} alt="" />}
           <p>{user.roles} </p>
         </div>
         <div className="infoCard">
           <p><PersonRoundedIcon fontSize="small"/> <span> {user.firstName} {user.lastName}</span></p>
-          <p><RestoreRoundedIcon fontSize="small" />{user.lastTimeOnline ? <p>{user.lastTimeOnline} </p> : <span> never</span>}</p>
+          <p><RestoreRoundedIcon fontSize="small" />{user.lastTimeOnline ? <p>{user.lastTimeOnline} </p> : <span>  Never</span>}</p>
           <p><LocationOnIcon fontSize="small" />{user.country}</p>
         </div>
         <div className="iconCard">
