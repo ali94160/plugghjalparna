83541640e7bf722a4dd5cf0cef22ebabbd335ab4
Ali94160 <ali.al-mousawi@hotmail.com>
2021-04-18 05:44:58 +0200

    deleted 2 modals

--- www/src/components/UserItem.js
+++ www/src/components/UserItem.js
@@ -12,7 +12,7 @@ import NotInterestedRoundedIcon from '@material-ui/icons/NotInterestedRounded';
 import { makeStyles } from '@material-ui/core/styles';
 import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 import Modal from '@material-ui/core/Modal';
-import ModalTemplate from './ModalTemplate';
+
 
 const UserItem = ({ user }) => {
 
@@ -31,6 +31,42 @@ const UserItem = ({ user }) => {
   const handleClose = () => {
     setOpen(false);
   };
+
+  // MODAL Start
+  function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 500,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    outline: 'none'
+  },
+}));
+
+
+
+    const classes = useStyles();
+  // Modal body
+  const [modalStyle] = useState(getModalStyle);
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <CloseRoundedIcon onClick={handleClose} />
+     
+    </div>
+  );
+  // Modal end
 
 
   return (
@@ -52,11 +88,16 @@ const UserItem = ({ user }) => {
         </div> 
       </div>
 
-      <ModalTemplate
-        handleOpen={handleOpen}
-        handleClose={handleClose}
-        open={open}
-      />
+            <div className="delete">
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="simple-modal-title"
+              aria-describedby="simple-modal-description"
+             >
+              {body}
+            </Modal>
+          </div>
           
     </div>
     );
