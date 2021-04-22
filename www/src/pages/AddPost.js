c32a689d998dc69bbd58b927e2c21686aaf39386
Ali94160 <ali.al-mousawi@hotmail.com>
2021-04-22 21:08:01 +0200

    create-post page styling and html fix.

--- www/src/pages/AddPost.js
+++ www/src/pages/AddPost.js
@@ -25,18 +25,9 @@ const AddPost = () => {
 
   return (
     <div className="addPost">
-      <h2>Create a Post</h2>
-       <div className="rules">
-        <ul>
-          <li>EJ svordomar</li>
-          <li>EJ Spam</li>
-          <li>EJ annat språk</li>
-        </ul>
-      </div>
-
       <div className="formWrapper">
       <form onSubmit={submitHandler}>
-      <h3>What type of recidense would you like to host?</h3>
+      <h3> Vilket ämne ska inlägget publiceras?</h3>
       <select value={subject} onChange={changeOptionHandler} required className="optionBar" >
         <option defaultValue="DEFAULT" disabled="disabled" >Subject?</option>
         <option>Allmänt</option>
@@ -55,11 +46,30 @@ const AddPost = () => {
         <input required ref={title} className="inputTitle" type="text" placeholder="T.ex [Hjälp] ekvationen 5y+x(5+43)..." />
 
         <p>Beskrivning</p> 
-        <textarea required  className="formTextBox" placeholder="Describe your residence..." name="w3review" rows="4" cols="50"></textarea>
+        <textarea required  className="formTextBox" placeholder="Skriv något här..." name="w3review" rows="6" cols="100"></textarea>
 
         <button className="formBtn">Create Post</button>
         </form>
-        </div>
+      </div>
+      <div className="rules">
+        <p>Regler av Inlägg</p>
+        <ul>
+          <span style={{fontSize: 18, fontWeight: 'bolder', color: 'darkred'}}>Förbjudet att:</span>
+          <li>Spamma inlägg</li>
+          <li>Annan marknadsföring</li>
+          <li>Rasism</li>
+          <li>Ovårdat språk</li>
+          <li>Mobbning och uthängning</li>
+          <li>18+ material</li>
+          <br></br>
+          <li>Genom att skapa inlägget godkänner du regler och villkor inom PluggHjälparna.
+          Vid regelbrott har vi rätten till att banna användaren eller ta bort inlägg.
+          </li>
+          <br/>
+          <li>Vid frågor vänligen kontakta vår Support.</li>
+        </ul>
+      </div>
+      
 
      
 
